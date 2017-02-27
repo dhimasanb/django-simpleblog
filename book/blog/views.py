2a755003115c6fb9e3293274eecafc51393f0a90
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-27 17:38:38 +0700

    add def blog

--- book/blog/views.py
+++ book/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
 def blogs(request):
     return render(request, 'blogs.html')
+
+def blog(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'blog.html', { 'blog': blog })
